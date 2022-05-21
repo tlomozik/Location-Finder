14c6cd01f36e184df72f6846cb9c1e9c2ed287c0
Tymek Łomozik <tymek994@gmail.com>
2022-05-21 09:26:22 +0200

    ignore error tag

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
+import { LogBox } from "react-native";
 import SearchScreen from "./src/screens/SearchScreen";
 import * as Localization from "expo-localization";
 import { pl, en } from "./src/components/languages";
@@ -9,7 +9,7 @@ import i18n from "i18n-js";
 i18n.fallbacks = true;
 i18n.translations = { pl, en };
 i18n.locale = Localization.locale;
-
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
